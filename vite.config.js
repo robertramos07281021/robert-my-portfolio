9ebbeca3724b9e0e46e9ddc3e4e58250837d588a
Robert Ramos <robert.ramos07281021@gmail.com>
2024-07-03 19:58:54 +0800

    navbar: menu and close button

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/robert-my-portfolio/"
+  base: "/robert-my-portfolio/",
+  assetsInclude: ['**/*.PNG']
 })

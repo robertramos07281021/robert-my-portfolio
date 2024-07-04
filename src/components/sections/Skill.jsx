cdf34242647943ff7c56f2dddcff3958bc9872ea
Robert Ramos <robert.ramos07281021@gmail.com>
2024-07-04 17:15:41 +0800

    skills

--- src/components/sections/Skill.jsx
+++ src/components/sections/Skill.jsx
@@ -10,12 +10,12 @@ const Skill = () => {
         <SkillsModal label='CSS' text="I can use flexbox and grid in layouting the pages, Utilize pseudo-classes and pseudo elements, create a animations and etc. I can use frameworks Bootstrap and Tailwind."/>
         <SkillsModal label='JS' text="Basic knowledge in functions, arrow functions, IIFE. Arrays and array methods. Objects, object properties, object methods, object constructor. I have basic knowledge in DOM manipulation."/>
         <SkillsModal label='MongoDB' text="I can create database using MongoDB, I can manipulate the data, how to populate, how to create a relationship between database models. "/>
-        <SkillsModal label='Express' text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum repellendus ex, fugiat praesentium, sequi ab eum quisquam natus rem aliquam temporibus tempora mollitia placeat minima. Accusantium iusto error eaque ipsa!"/>
-        <SkillsModal label='NodeJS' text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum repellendus ex, fugiat praesentium, sequi ab eum quisquam natus rem aliquam temporibus tempora mollitia placeat minima. Accusantium iusto error eaque ipsa!"/>
-        <SkillsModal label='ReactJS' text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum repellendus ex, fugiat praesentium, sequi ab eum quisquam natus rem aliquam temporibus tempora mollitia placeat minima. Accusantium iusto error eaque ipsa!"/>
-        <SkillsModal label='Comm' text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum repellendus ex, fugiat praesentium, sequi ab eum quisquam natus rem aliquam temporibus tempora mollitia placeat minima. Accusantium iusto error eaque ipsa!"/>
-        <SkillsModal label='Problem Solving' text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum repellendus ex, fugiat praesentium, sequi ab eum quisquam natus rem aliquam temporibus tempora mollitia placeat minima. Accusantium iusto error eaque ipsa!"/>
-        <SkillsModal label='Learning' text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum repellendus ex, fugiat praesentium, sequi ab eum quisquam natus rem aliquam temporibus tempora mollitia placeat minima. Accusantium iusto error eaque ipsa!"/>
+        <SkillsModal label='Express' text="I can create middlewares, error handlers routing and http methods, i can easyly connect my app to databases how to have a good RESTFULL API."/>
+        <SkillsModal label='NodeJS' text="I can efficiently handles thousands of current connection to avoids the complexities of managing thread concurrency that can bring a bug, I can have a full stack website on this server, just add express and mongodb techstack."/>
+        <SkillsModal label='ReactJS' text="I can prepare a good file arrangement, I can use hooks, create a components, I can use props, proptypes, default props value."/>
+        <SkillsModal label='Comm' text="I can communicate to other software engineers, and team to other, can borrow any kind of suggestions."/>
+        <SkillsModal label='Problem Solving' text="I cannot assure if I can solve the problem but I can assure i will find the answer on that problem, I can work in critical situations, I work in agile environment."/>
+        <SkillsModal label='Learning' text="I am not a fast learner but I am willing to learn from others whether it is jr, middle, or senior software engineers, and I am willing to adopt other tech stacks, and I always want to stay a student."/>
       </div>
     </article>
   )

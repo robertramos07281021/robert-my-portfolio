f542576f7cc02498d01370ab9d3219892d12d2cb
Robert Ramos <robert.ramos07281021@gmail.com>
2024-07-04 10:52:10 +0800

    certImage

--- src/components/sections/Certificate.jsx
+++ src/components/sections/Certificate.jsx
@@ -7,7 +7,7 @@ const Certificate = () => {
     <article className="h-5/6 w-full max-sm:p-5">
       <h1 className="text-4xl font-bold tracking-widest">Certificate<span className="text-lime-500">s</span></h1>
       <div className=" h-5/6 w-full bg-white/10 flex max-sm:justify-center flex-wrap gap-5 rounded p-2 overflow-y-auto overflow-hidden mt-9">
-      <CertCards title="KodeGo Cert" desc="Full Stack Web Dev" year="2023" href='http://localhost:5173/robert-my-portfolio/src/assets/KodeGo-CERT.PNG' src={getImageUrl('KodeGo-CERT.PNG')}/>
+      <CertCards title="KodeGo Cert" desc="Full Stack Web Dev" year="2023" href='' src={getImageUrl('KodeGo-CERT.PNG')}/>
       </div>
     </article>
   )
